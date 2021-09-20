91d61bf05283060decb122dfec9cdcc088a85aac
micheelRojas <56304710+micheelRojas@users.noreply.github.com>
2021-09-20 00:01:56 -0500

    Test del H8 en verde

--- BancoTDD.Dominio.Test/CDTs/CDTTest.cs
+++ BancoTDD.Dominio.Test/CDTs/CDTTest.cs
@@ -39,7 +39,35 @@ namespace BancoTDD.Dominio.Test.CDTs
             Assert.AreEqual("Su Nuevo Saldo es de $ 1.000.000,00 pesos m/c", respuesta);
         }
 
-       
+        /*
+         * HU 8.
+            Como Usuario quiero el retiro de mi dinero de mi CDT al finalizar el Término establecido para
+            recuperar el dinero depositado.
+            Criterios de Aceptación
+            8.1 Los retiros sólo se podrán realizar una vez haya finalizado el término del CDT.
+            8.2 Al realizar el retiro se le liquidará un interés de acuerdo a la tasa definida y plazo de termino.
+            8.3 El valor a retirar se reduce del saldo del CDT.
+         */
+
+        /*
+         *  Dado El cliente tiene una tarjeta de CDT a termino trimestral con un saldo de 1 millon de pesos 
+         Cuando va a reirar $1.000.050,00 pesos pasado los 3 meses .
+         Entonces El sistema registrará el retiro se le liquidará un interés de acuerdo a la tasa definida de 3% y plazo de termino de trimestre.
+         AND presentará el mensaje. “Su Nuevo Saldo es de $ 1.014.673,84 pesos m/c"
+         */
+        [Test]
+        public void PuedeRetirarCorrecta()
+        {
+            var cdt = new CDT(numero: "10001", nombre: "Cuenta Ejemplo", termino: "Trimestre", tasa: 0.06);
+            decimal valorConsignacion = 1000000;
+            cdt.Consignar(valorConsignacion: valorConsignacion, fecha: new DateTime(2020, 2, 1));
+            
+            decimal valorRetirar= 1000000;
+            string respuesta = cdt.Retirar(valorRetiro: valorRetirar, fecha: new DateTime(2020, 5, 1));
+            
+            Assert.AreEqual("Su Nuevo Saldo es de $ 14.673,85 pesos m/c", respuesta);
+        }
+
     }
 
     internal class CDT
@@ -76,32 +104,66 @@ namespace BancoTDD.Dominio.Test.CDTs
 
             throw new NotImplementedException();
         }
+        internal string Retirar(decimal valorRetiro, DateTime fecha)
+        {
+            decimal SaldoTemporal= (Convert.ToDecimal(CalcularTermino(this.Termino, this.Tasa)) + 1) * Saldo;
+            if (DifenciaMeses(fecha, MovimientoAnterior(this._movimientos).Fecha) == this.Termino && valorRetiro<= SaldoTemporal)
+            {
+                _movimientos.Add(new Movimiento(cdt: this, fecha: fecha, tipo: "RETIRO", valor: valorRetiro));
+                Saldo = SaldoTemporal-valorRetiro;
+                return $"Su Nuevo Saldo es de {Saldo:c2} pesos m/c";
+            }
+
+            throw new NotImplementedException();
+        }
         internal static string DifenciaMeses(DateTime fechaFin, DateTime fechaInicio) {
             decimal diferencia = Math.Abs((fechaFin.Month - fechaInicio.Month) + 12 * (fechaFin.Year - fechaInicio.Year));
-            if (diferencia >= 12) {
+            if (diferencia >= 12)
+            {
                 return "Anual";
             }
-            else if (diferencia >= 6) {
+            else if (diferencia >= 6)
+            {
                 return "Semestre";
             }
-            else if (diferencia >= 3) {
-                return "Trimestre"; 
+            else if (diferencia >= 3)
+            {
+                return "Trimestre";
+            }
+            else if (diferencia > 1) {
+                return "Mensual";
             }
                 return " " ;
         }
         internal Movimiento MovimientoAnterior(List<Movimiento> movimiento)
         {
-
-            int cont = 0;
-            Movimiento ultimoMovimiento= null;
+            Movimiento ultimoMovimiento=null;
 
             for (int i = 0; i < movimiento.LongCount(); i++)
             {
-                if (movimiento[i].Tipo.Equals("Consignacion"))
+                if (movimiento[i].Tipo.Equals("CONSIGNACION"))
                     ultimoMovimiento=movimiento[i];
             }
             return ultimoMovimiento;
         }
+
+        internal double CalcularTermino(string termino, double tasa) {
+            double Te=0.0;
+            if (termino.Equals("Mensual")) {
+                Te = (Math.Pow((1+tasa), 0.0833333333333333))-1;
+            }
+            if (termino.Equals("Trimestre")) {
+                Te = (Math.Pow((1 + tasa), 0.25)) - 1;
+            }
+            if (termino.Equals("Semestre")) {
+                Te = (Math.Pow((1 + tasa), 0.5)) - 1;
+            }
+            if (termino.Equals("Anual")) {
+                Te = (Math.Pow((1 + tasa), 1)) - 1;
+            }
+            return Te;
+        }
+       
     }
     internal class Movimiento
     {
